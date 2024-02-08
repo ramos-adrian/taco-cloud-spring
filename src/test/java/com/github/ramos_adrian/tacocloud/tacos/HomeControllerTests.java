20660cb7f283a5b267c3dba265865cea9da50fbf
Ramos Adrián <ramosadrian@pm.me>
2024-02-08 11:51:47 -0300

    Create home page

---
+++ src/test/java/com/github/ramos_adrian/tacocloud/tacos/HomeControllerTests.java
@@ -0,0 +1,35 @@
+package com.github.ramos_adrian.tacocloud.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static
+org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static
+org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.hamcrest.Matchers.containsString;
+
+import com.github.ramos_adrian.tacocloud.taco.HomeController;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+
+    private final MockMvc mockMvc;
+
+    public HomeControllerTests(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("home"))
+        .andExpect(content().string(containsString("Welcome to ...")));
+    }
+
+}
