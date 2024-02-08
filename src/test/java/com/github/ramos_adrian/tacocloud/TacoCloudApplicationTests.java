c6f09b2bca5b3ffe48ce2eb2cd7f728e758cc957
Ramos Adrián <ramosadrian@pm.me>
2024-02-08 10:41:37 -0300

    Setup new Spring project

---
+++ src/test/java/com/github/ramos_adrian/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ramos_adrian.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
